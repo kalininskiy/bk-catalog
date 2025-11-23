0683208b7933fd2a0b41ba749c648ba9484e0f17
Ivan Kalininskiy <kalininskiy_iv@albank.ru>
2025-11-23 14:51:01 +0900

    fix #8

--- scripts/images.js
+++ scripts/images.js
@@ -1,8 +1,12 @@
-/**
- * @file Модуль для модального окна с изображением
- * @author Иван "VDM" Kalininskiy <https://t.me/VanDamM>
- * @copyright 2025
- * @license GNU GPLv3
+/*
+ * Copyright (C) 2025 Иван "VDM" Kalininskiy <https://t.me/VanDamM>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
 document.addEventListener('DOMContentLoaded', () => {
--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,14 @@
-// main.js
+/*
+ * Copyright (C) 2025 Иван "VDM" Kalininskiy <https://t.me/VanDamM>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 document.addEventListener('DOMContentLoaded', () => {
     console.log('main.js');
 });
